54cc94e37661e7d299eadb976bb4e15935ec0512
Leonardo César <leonardocesar@usp.br>
2025-02-17 20:42:51 -0300

    Implementa a função delete, assim como corrige os erros de contagem de tasks das outras funções. Fim do projeto

--- ToDo_list.py
+++ ToDo_list.py
@@ -1,11 +1,13 @@
 class Task:
-    number_of_tasks = 0     #class atributes
 
     def __init__(self, name, status, deadline):
         self.name = name
         self.status = status
         self.deadline = deadline
         Task.number_of_tasks += 1
+
+    def __str__(self):
+        return f"Name: {self.name}, Status: {self.status}, Deadline: {self.deadline}"
 
 class ToDo_list:
     number_of_finished_tasks = 0        #class atributes
@@ -21,6 +23,8 @@ class ToDo_list:
                 ToDo_list.number_of_finished_tasks += 1
             else:
                 ToDo_list.number_of_unfinished_tasks += 1
+        return
+        
 
     def add(self, task_name, task_status, task_deadline):
         #this method adds a new task to Todo_list
@@ -31,8 +35,13 @@ class ToDo_list:
                 print(" ")
                 return
         self.tasks.append(new_task)
-        print(f"{new_task.name} is now on your list")
+        if(new_task.status == "finished"):
+                ToDo_list.number_of_finished_tasks += 1
+        else:
+            ToDo_list.number_of_unfinished_tasks += 1
+        print(f"{new_task} is now on your list")
         print(" ")
+        return
 
     def update(self, task_name, new_status, new_deadline):
         #this method update a task that already is in Todo_list
@@ -41,6 +50,9 @@ class ToDo_list:
                 if(task.status != new_status):
                     print(f"{task.name} is now {new_status}")
                 task.status = new_status
+                print(task.status)
+                print(new_status)
+                print(new_status == "finished")
                 if(task.deadline != new_deadline and new_deadline != " "):
                     print(f"{task.name} deadline is now {new_deadline}")
                 task.deadline = new_deadline
@@ -54,6 +66,35 @@ class ToDo_list:
                 return
         print(f"{task_name} is not in your list yet")
         print(" ")
+        return
+
+    
+    def show(self):
+        print(f"You have {ToDo_list.number_of_finished_tasks} finished tasks")          
+        print(f"YOu have {ToDo_list.number_of_unfinished_tasks} unfinished tasks")
+        print(" ")
+        print(f"These are the tasks on the current list:")
+        for task in self.tasks:
+            print(task)
+        print(" ")
+        return
+
+    def delete(self, remove_task_name):
+        for task in self.tasks:
+            if (remove_task_name == task.name):
+                if(task.status == "finished"):
+                    ToDo_list.number_of_finished_tasks -= 1
+                else:
+                    ToDo_list.number_of_unfinished_tasks -= 1
+                self.tasks.remove(task)
+                print(f"{remove_task_name} was removed from your tasks")
+                print(" ")
+                return
+        print(f"{remove_task_name} can't be removed because it's not in your tasks")
+        print(" ")
+        return
+
+
 
 
 #client test
@@ -65,9 +106,15 @@ my_list = ToDo_list([task1, task2, task3])
 my_list.add("shopping", "unfinished", "05/02")
 my_list.add("japanese_lesson", "unfinished", "06/02")         #shouldnt add
 
-my_list.update("groceries", "finished", " ")
-my_list.update("coding_project", "unfinished", "20/06")
+my_list.update("groceries", "finished", "05/02")
+my_list.update("coding_project", "finished", "20/06")
 my_list.update("jogging", "unfinished", "05/02")              #shouldnt update
+
+my_list.show()
+
+my_list.delete("groceries")   
+my_list.delete("homework")  
+my_list.show()          #without groceries
 
 
 
